d720e3bdaf7141c0e66adb3cb48aa2256d3f9892
Stefan Haninger <haninger@imfusion.com>
2021-07-28 15:21:52 +0200

    Initial commit with vimrc + cpp configs + ubuntu installation for vim

---
+++ ubuntu/install_vim.sh
@@ -0,0 +1,6 @@
+sudo apt install vim ctags nodejs npm silversearcher-ag
+# if nodejs < 10.12 install nvm
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
