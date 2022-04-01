a2f9d7da173ade1e25865bcc7e5fc6004b6059cc
Stefan Haninger <haninger@imfusion.com>
2022-04-01 13:59:07 +0200

    reactivate redo shortcut by moving RUN shortcut to Ctrl-X

--- neovim/lua/imfusioncpp.lua
+++ neovim/lua/imfusioncpp.lua
@@ -3,7 +3,7 @@ vim.cmd ("set makeprg=mold\\ -run\\ ninja\\ -C\\ " .. folder)
 local opts = { noremap = true, silent = true}
 local keymap = vim.api.nvim_set_keymap
 keymap("n", "<C-B>", ":make!<CR>", opts)
-keymap("n", "<C-R>", "<cmd>!./" .. folder .. "/bin/ImFusionSuite<CR>", opts)
+keymap("n", "<C-X>", "<cmd>!./" .. folder .. "/bin/ImFusionSuite<CR>", opts)
 keymap("n", "<C-N>", ":Files<CR>", opts)
 keymap("n", "<C-F>", ":BLines<CR>", opts)
 keymap("n", "<S-F>", ":Ag<CR>", opts)
