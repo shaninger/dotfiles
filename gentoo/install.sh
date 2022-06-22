1ca4f199dba06bf78f4547614e47433ac440167c
Stefan <stefan.haninger@yahoo.de>
2022-06-22 18:31:50 +0200

    [gentoo] added i3 config, make.conf, polybar config, modified xinitrc and install script

--- gentoo/install.sh
+++ gentoo/install.sh
@@ -10,7 +10,8 @@ emerge neovim
 # make sure X is in USE flag
 emerge x11-wm/i3-gaps
 emerge x11-misc/i3lock
-emerge x11-misc/polybar
+# make use it is emerged with i3 support
+USE="i3wm" emerge x11-misc/polybar
 
 emerge dev-vcs/git 
 
@@ -18,3 +19,18 @@ emerge dev-vcs/git
 emerge app-portage/gentoolkit
 
 emerge www-client/firefox
+
+emerge x11-misc/picom
+#make sure to compile with jpeg USE flag
+USE="jpeg" emerge x11-misc/nitrogen
+emerge x11-misc/dmenu
+
+#install siji fonts
+# make sure to add corresponding lines to xinitrc
+# use "fc-list | grep siji" to find out the name
+# copy the name into polybar config
+# it works if "fc-match NAME" does find the correct font file
+# reload font cache: "fc-cache -fv"
+git clone https://github.com/stark/siji && cd siji
+./install.sh
+cd
