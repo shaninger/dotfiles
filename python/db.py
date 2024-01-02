4a6c783dca5d7eafa68c5a0faed18e2602ad4d5c
Stefan Haninger <haninger@imfusion.com>
2024-01-02 07:13:05 +0100

    [DB] format link str + allow multiple times per day

--- python/db.py
+++ python/db.py
@@ -60,12 +60,20 @@ def search_in_html(html_content):
     return prices, duration
 
 def create_link_to_berlin(date, time):
-    return f'https://www.bahn.de/buchung/fahrplan/suche#sts=true&so=M%C3%BCnchen%20Hbf&zo=Berlin%20Hbf&kl=2&r=13:16:KLASSENLOS:1&soid=A%3D1%40O%3DM%C3%BCnchen%20Hbf%40X%3D11558339%40Y%3D48140229%40U%3D81%40L%3D8000261%40B%3D1%40p%3D1702504538%40i%3DU%C3%97008020347%40&zoid=A%3D1%40O%3DBerlin%20Hbf%40X%3D13369549%40Y%3D52525589%40U%3D81%40L%3D8011160%40B%3D1%40p%3D1702504538%40i%3DU%C3%97008065969%40&sot=ST&zot=ST&soei=8000261&zoei=8011160&hd={date}T{time}:36&hza=D&ar=false&s=true&d=true&hz=%5B%5D&fm=false&bp=false'
+    return f'https://www.bahn.de/buchung/fahrplan/suche#sts=true&so=M%C3%BCnchen%20Hbf&zo=Berlin%20Hbf&kl=2&r=13:16:KLASSENLOS' \
+           + f':1&soid=A%3D1%40O%3DM%C3%BCnchen%20Hbf%40X%3D11558339%40Y%3D48140229%40U%3D81%40L%3D8000261%40B%3D1%40p%3D1702504538' \
+           + f'%40i%3DU%C3%97008020347%40&zoid=A%3D1%40O%3DBerlin%20Hbf%40X%3D13369549%40Y%3D52525589%40U%3D81%40L%3D8011160%40B%3D1' \
+           + f'%40p%3D1702504538%40i%3DU%C3%97008065969%40&sot=ST&zot=ST&soei=8000261&zoei=8011160&hd={date}T{time}:36&hza=D&ar=false' \
+           + f'&s=true&d=true&hz=%5B%5D&fm=false&bp=false'
 
 def create_link_to_munich(date, time):
-    return f'https://www.bahn.de/buchung/fahrplan/suche#sts=true&so=Berlin%20Hbf&zo=M%C3%BCnchen%20Hbf&kl=2&r=13:16:KLASSENLOS:1&soid=A%3D1%40O%3DBerlin%20Hbf%40X%3D13369549%40Y%3D52525589%40U%3D81%40L%3D8011160%40B%3D1%40p%3D1702504538%40i%3DU%C3%97008065969%40&zoid=A%3D1%40O%3DM%C3%BCnchen%20Hbf%40X%3D11558339%40Y%3D48140229%40U%3D81%40L%3D8000261%40B%3D1%40p%3D1702504538%40i%3DU%C3%97008020347%40&sot=ST&zot=ST&soei=8011160&zoei=8000261&hd={date}T{time}:36&hza=D&ar=false&s=true&d=true&hz=%5B%5D&fm=false&bp=false'
+    return f'https://www.bahn.de/buchung/fahrplan/suche#sts=true&so=Berlin%20Hbf&zo=M%C3%BCnchen%20Hbf&kl=2&r=13:16:KLASSENLOS' \
+           + f':1&soid=A%3D1%40O%3DBerlin%20Hbf%40X%3D13369549%40Y%3D52525589%40U%3D81%40L%3D8011160%40B%3D1%40p%3D1702504538' \
+           + f'%40i%3DU%C3%97008065969%40&zoid=A%3D1%40O%3DM%C3%BCnchen%20Hbf%40X%3D11558339%40Y%3D48140229%40U%3D81%40L%3D8000261' \
+           + f'%40B%3D1%40p%3D1702504538%40i%3DU%C3%97008020347%40&sot=ST&zot=ST&soei=8011160&zoei=8000261&hd={date}T{time}:36&hza=D&ar=false' \
+           + f'&s=true&d=true&hz=%5B%5D&fm=false&bp=false'
 
-def calculate_trip(start_date, end_date, direction='munich_to_berlin', trip_days=[5, 6, 7], time='17:00',
+def calculate_trip(start_date, end_date, direction='munich_to_berlin', trip_days=[5, 6, 7], times=['17:00'],
                    max_duration=5) -> list[
     tuple[float, dict[str, float]]]:
     if not direction in ['munich_to_berlin', 'berlin_to_munich']:
@@ -75,29 +83,25 @@ def calculate_trip(start_date, end_date, direction='munich_to_berlin', trip_days
     all_prices_to_berlin = {}
     for trip_forth_date in (start_date + timedelta(n) for n in range(day_count)):
         date = trip_forth_date.strftime("%Y-%m-%d")
-        trip_to_berlin = create_link_to_berlin(date, time)
-        trip_to_munich = create_link_to_munich(date, time)
+        for time in times:
+            if len(time) != 5:
+                raise ValueError('Time has to be in the format: HH:MM')
+            trip_to_berlin = create_link_to_berlin(date, time)
+            trip_to_munich = create_link_to_munich(date, time)
 
-        html_content = selenium_scraper(trip_to_berlin)
-        if not html_content is None:
-            prices_to_berlin, duration_to_berlin = search_in_html(html_content)
-            cheapest_price = min(zip(prices_to_berlin, duration_to_berlin), key=lambda x: x[0] if x[1] < max_duration else float('inf')) if len(
-                prices_to_berlin) > 0 and len(duration_to_berlin) > 0 else (None, None)
-            print(
-                f'Trip Munich to Berlin -> day: {date}, cheapest price: {cheapest_price[0]}{"€" if not cheapest_price[0] is None else ""}' \
-                + f', duration: {cheapest_price[1]}{"h" if not cheapest_price[1] is None else ""}')
-            all_prices_to_berlin[date] = cheapest_price
+            def parse_single_trip(trip, prices, trip_name):
+                html_content = selenium_scraper(trip)
+                if not html_content is None:
+                    prices_to_berlin, duration_to_berlin = search_in_html(html_content)
+                    cheapest_price = min(zip(prices_to_berlin, duration_to_berlin), key=lambda x: x[0] if x[1] < max_duration else float('inf')) if len(
+                        prices_to_berlin) > 0 and len(duration_to_berlin) > 0 else (None, None)
+                    print(
+                        f'Trip {trip_name} -> day: {date}, cheapest price: {cheapest_price[0]}{"€" if not cheapest_price[0] is None else ""}' \
+                        + f', duration: {cheapest_price[1]}{"h" if not cheapest_price[1] is None else ""}')
+                    prices[date] = cheapest_price
 
-        html_content = selenium_scraper(trip_to_munich)
-        if not html_content is None:
-            prices_to_munich, duration_to_munich = search_in_html(html_content)
-            cheapest_price = min(zip(prices_to_munich, duration_to_munich), key=lambda x: x[0] if x[1] < max_duration else float('inf')) if len(
-                prices_to_munich) > 0 and len(duration_to_munich) > 0 else (None, None)
-            print(
-                f'Trip Berlin to Munich -> day: {date}, cheapest price: {cheapest_price[0]}{"€" if not cheapest_price[0] is None else ""}' \
-                + f', duration: {cheapest_price[1]}{"h" if not cheapest_price[1] is None else ""}')
-
-            all_prices_to_munich[date] = cheapest_price
+            parse_single_trip(trip_to_berlin, all_prices_to_berlin, 'Munich to Berlin')
+            parse_single_trip(trip_to_munich, all_prices_to_munich, 'Berlin to Munich')
 
     complete_trips = []
     if direction == 'munich_to_berlin':
@@ -135,7 +139,7 @@ if __name__ == "__main__":
 
     start_date_hanka = date(2024, 3, 22)
     end_date_hanka = date(2024, 4, 7)
-    trip_hanka = calculate_trip(start_date_hanka, end_date_hanka, 'berlin_to_munich', [5, 6, 7])
+    trip_hanka = calculate_trip(start_date_hanka, end_date_hanka, 'berlin_to_munich', [5, 6, 7, 8], times=['10:00', '13:30', '17:00'])
 
     def print_trip(trip):
         for k, v in sorted(trip, key=lambda x: x[0]):
